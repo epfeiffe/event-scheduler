984471834acfa1d70c178676455f00e303b17655
Emerson Pfeiffer <epfeiffe@umich.edu>
2025-11-02 11:33:20 -0800

    Fix deployment issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,
+  } as any, // cast to any to suppress type error,
 };
 
 export default nextConfig;
